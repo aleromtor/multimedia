0bba954ee11d617e5edde70dbba78ce7505b12de
PaulaCuenca <103761543+PaulaCuenca@users.noreply.github.com>
2024-02-20 18:34:14 +0100

    furbol

--- js/game.js
+++ js/game.js
@@ -396,7 +396,7 @@ var levels = {
 	// Datos de nivel
 	data:[
 	 {   // Primer nivel 
-		foreground:'desert-foreground',
+		foreground:'stadium',
 		background:'clouds-background',
 		entities:[
 			{type:"ground", name:"dirt", x:500,y:440,width:1000,height:20,isStatic:true},
@@ -404,18 +404,18 @@ var levels = {
 
 			{type:"block", name:"wood", x:520,y:380,angle:90,width:100,height:25},
 			{type:"block", name:"glass", x:520,y:280,angle:90,width:100,height:25},								
-			{type:"villain", name:"burger",x:520,y:205,calories:590},
+			{type:"villain", name:"iug",x:520,y:205,calories:590},
 
 			{type:"block", name:"wood", x:620,y:380,angle:90,width:100,height:25},
 			{type:"block", name:"glass", x:620,y:280,angle:90,width:100,height:25},								
 			{type:"villain", name:"fries", x:620,y:205,calories:420},				
 
 			{type:"hero", name:"orange",x:80,y:405},
-			{type:"hero", name:"apple",x:140,y:405},
+			{type:"hero", name:"ball",x:140,y:405},
 		]
 	 },
 		{   // Segundo nivel
-			foreground:'desert-foreground',
+			foreground:'stadium',
 			background:'clouds-background',
 			entities:[
 				{type:"ground", name:"dirt", x:500,y:440,width:1000,height:20,isStatic:true},
@@ -591,6 +591,25 @@ var entities = {
 			friction:0.5,
 			restitution:0.4,	
 		},
+
+		"ball":{
+			shape:"circle",
+			radius:25,
+			density:1.5,
+			friction:0.5,
+			restitution:0.4,	
+		},
+
+		"iug":{
+			shape:"circle",
+			fullHealth:40,
+			radius:25,
+			density:1,
+			friction:0.5,
+			restitution:0.4,	
+		},
+
+		
 	},
 	// Tomar la entidad, crear un cuerpo box2d y añadirlo al mundo
 	create:function(entity){
